0a9aaca67a41c7a967ee3a1606b1058888c95334
Paulo DAlberti <phdalberti@gmail.com>
2020-05-30 23:30:15 +0900

    added font awesome and layout

--- template.rb
+++ template.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# GEMS
+################################################################################
+
 gem 'autoprefixer-rails'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'friendly_id', '~> 5.3'
@@ -20,18 +23,49 @@ gem_group :development, :test do
   gem 'spring'
 end
 
+# LAYOUT
+################################################################################
+
+remove_file 'app/assets/stylesheets/application.css'
+file 'app/assets/stylesheets/application.css.scss', <<-SCSS
+  @import "font-awesome-sprockets";
+  @import "font-awesome";
+SCSS
+
+run 'rm app/views/layouts/application.html.erb'
+file 'app/views/layouts/application.html.erb', <<-HTML
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <title>TODO</title>
+      <%= csrf_meta_tags %>
+      <%= action_cable_meta_tag %>
+      <%= stylesheet_link_tag 'application', media: 'all' %>
+      <%#= stylesheet_pack_tag 'application', media: 'all' %> <!-- Uncomment if you import CSS in app/javascript/packs/application.js -->
+    </head>
+    <body>
+      <%= yield %>
+      <%= javascript_pack_tag 'application' %>
+    </body>
+  </html>
+HTML
+
+# AFTER BUNDLE
+################################################################################
+
 after_bundle do
-  generate('annotate:install')
-  generate('simple_form:install')
+  generate 'annotate:install'
+  generate 'simple_form:install'
   # rails generate simple_form:install --bootstrap
   # rails generate simple_form:install --foundation
-  rails_command('sitemap:install')
-  run('bundle exec spring binstub --all')
+  rails_command 'sitemap:install'
+  run 'bundle exec spring binstub --all'
 
-  rails_command('db:create')
-  rails_command('db:migrate')
+  rails_command 'db:create db:migrate'
 
-  git(:init)
-  git(add: '.')
-  git(commit: %( -m 'Initial commit using personal-projects-template' ))
+  git :init
+  git add: '.'
+  git commit: %( -m 'Initial commit using personal-projects-template' )
 end
