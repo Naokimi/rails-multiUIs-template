86408e572d76530c0b2343e2c1dec3eefede6e35
Paulo DAlberti <phdalberti@gmail.com>
2020-06-03 21:43:44 +0900

    added navbar and footer

--- template.rb
+++ template.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
-# next step: implement one of the frameworks (bulma?)
-# next steo: implement second framework + framework choice option
+# add navbar and footer to bulma
+# next step: implement second framework + framework choice option
+# next step: move methods into support folder/file
 
 # GEMS
 ################################################################################
 
 gem 'autoprefixer-rails'
-gem "bulma-rails", "~> 0.8.2"
+gem 'bulma-rails', '~> 0.8.2'
 gem 'font-awesome-sass', '~> 5.12.0'
 gem 'friendly_id', '~> 5.3'
 gem 'mini_magick'
@@ -50,7 +51,78 @@ file 'app/views/layouts/application.html.erb', <<-HTML
       <%= stylesheet_link_tag 'application', media: 'all' %>
     </head>
     <body>
+      <nav class="navbar" role="navigation" aria-label="main navigation">
+        <div class="navbar-brand">
+          <a class="navbar-item" href="https://bulma.io">
+            <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
+          </a>
+
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-start">
+            <a class="navbar-item">
+              Home
+            </a>
+
+            <a class="navbar-item">
+              Documentation
+            </a>
+
+            <div class="navbar-item has-dropdown is-hoverable">
+              <a class="navbar-link">
+                More
+              </a>
+
+              <div class="navbar-dropdown">
+                <a class="navbar-item">
+                  About
+                </a>
+                <a class="navbar-item">
+                  Jobs
+                </a>
+                <a class="navbar-item">
+                  Contact
+                </a>
+                <hr class="navbar-divider">
+                <a class="navbar-item">
+                  Report an issue
+                </a>
+              </div>
+            </div>
+          </div>
+
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>Sign up</strong>
+                </a>
+                <a class="button is-light">
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </nav>
       <%= yield %>
+      <footer class="footer">
+        <div class="content has-text-centered">
+          <p>
+            <strong>Bulma</strong> by <a href="https://jgthms.com">Jeremy Thomas</a>. The source code is licensed
+            <a href="http://opensource.org/licenses/mit-license.php">MIT</a>.
+          </p>
+          <p>
+            Rails template made by <a href="https://github.com/Naokimi"><i class="fab fa-github"></i> Naokimi</a>
+          </p>
+        </div>
+      </footer>
       <%= javascript_pack_tag 'application' %>
     </body>
   </html>
@@ -59,17 +131,19 @@ HTML
 def overwrite_homepage
   run 'rm app/views/pages/home.html.erb'
   file 'app/views/pages/home.html.erb', <<-HTML
-    <section class="section">
-      <div class="container">
-        <h1 class="title">
-          <i class="fas fa-heart"></i> Bulma Framework <i class="fas fa-heart"></i>
-        </h1>
-        <p class="subtitle">
-          Discover how to <strong>customize</strong> it!
-        </p>
-        <p>
-          <a class="button is-light" href="https://bulma.io/documentation/overview/start/">Documentation</a>
-        </p>
+    <section class="hero is-primary">
+      <div class="hero-body">
+        <div class="container">
+          <h1 class="title">
+            <i class="fas fa-heart"></i> Bulma Framework <i class="fas fa-heart"></i>
+          </h1>
+          <h2 class="subtitle">
+            A framework without any JavaScript. Discover how to <strong>customize</strong> it!
+          </h2>
+          <p>
+            <a class="button is-light" href="https://bulma.io/documentation/overview/start/">Documentation</a>
+          </p>
+        </div>
       </div>
     </section>
   HTML
